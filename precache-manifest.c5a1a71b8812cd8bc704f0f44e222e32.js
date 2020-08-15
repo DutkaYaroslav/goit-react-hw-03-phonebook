2b7f8360c92f58a62eb685c1dc1a5741acc55c15
unknown <yaroslavdutka@gmail.com>
2020-08-15 12:41:45 +0300

    Updates

---
+++ precache-manifest.c5a1a71b8812cd8bc704f0f44e222e32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d2a40d0b3826fabedd6865c9bdcacd",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "27a57b4e6aa2e54eea77",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.754ac2ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.754ac2ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7760714e7a4888e0a2",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.197cc62f.chunk.js"
+  },
+  {
+    "revision": "356906f07e3d724d1528",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.d720b385.js"
+  }
+]);
